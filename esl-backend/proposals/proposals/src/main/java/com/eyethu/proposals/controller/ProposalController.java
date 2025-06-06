13e6eb49baf534c7bb2daa7cca5a9b03100b7532
sifiso <sifisoxmhlongo@gmail.com>
2025-06-06 19:34:07 +0200

    rename proposals service

--- esl-backend/proposals/proposals/src/main/java/com/eyethu/proposals/controller/ProposalController.java
+++ esl-backend/proposals/proposals/src/main/java/com/eyethu/proposals/controller/ProposalController.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/proposals")
 public class ProposalController {
+    
     @Autowired
     private ProposalRepository proposalRepository;
     @Autowired
